5566c2da5fb83f3cd8f1ace475d6e517ef017109
Justin Yoo <justin.yoo@aliencube.com>
2015-11-30 14:51:06 +1100

    Add Web API app project

---
+++ src/FilteringSample.ApiApp/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+using FilteringSample.ApiApp;
+
+namespace DevKimchi.FilteringSample.ApiApp
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
